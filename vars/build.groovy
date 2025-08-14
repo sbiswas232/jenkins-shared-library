b0bbd00cf974b0f261baa97647f1a317024f8f2b
sbiswas232 <139542832+sbiswas232@users.noreply.github.com>
2025-08-15 00:03:40 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,6 +1,6 @@
 def call(String ImageName, String ImageTag, String DockerhubUser) {
   sh'''
-  "docker build -t ${DockerhubUser}/${ImageName}:${ImageTag} ."
-  "docker run -d -p 80:80 ${DockerhubUser}/${ImageName}:${ImageTag}"
+  docker build -t "${DockerhubUser}/${ImageName}:${ImageTag}" .
+  docker run -d -p 80:80 "${DockerhubUser}/${ImageName}:${ImageTag}"
   '''
 }
