56d689ca99325918f2d33c96d78703c7a703de07
sbiswas232 <139542832+sbiswas232@users.noreply.github.com>
2025-08-15 00:41:38 +0530

    Update image.groovy

--- vars/image.groovy
+++ vars/image.groovy
@@ -2,5 +2,6 @@ def call (String ImageName, String ImageTag, String DockerhubUser) {
   sh"""
   docker ps
   docker build -t "${DockerhubUser}"/"${ImageName}":"${ImageTag}" .
+  docker run -d -p 80:80 "${DockerhubUser}"/"${ImageName}":"${ImageTag}"
   """
 }
