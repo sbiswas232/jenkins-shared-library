80ad0f017f5a5c44f1705cbd680f77e29cd81b82
sbiswas232 <139542832+sbiswas232@users.noreply.github.com>
2025-08-15 23:46:48 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,10 +1,10 @@
-def call(String ImageName, String ImageTag, String DockerhubUsr, String DockerhubPass, String ContainerName){
+def call(String ImageName, String ImageTag, String DockerhubUsr, String DockerhubPass, String ContainerName, String PortNum){
   sh"""
   docker build -t "${ImageName}":"${ImageTag}" .
   docker tag "${ImageName}":"${ImageTag}" "${DockerhubUsr}"/"${ImageName}":"${ImageTag}"
   echo "${DockerhubPass}" | docker login -u "${DockerhubUsr}" --password-stdin docker.io
   docker push "${DockerhubUsr}"/"${ImageName}":"${ImageTag}"
   docker logout
-  docker run -d -p --name "${ContainerName}" -p 80:80 "${ImageName}":"${ImageTag}"
+  docker run -d --name "${ContainerName}" -p "${PortNum}" "${ImageName}":"${ImageTag}"
   """
 }
