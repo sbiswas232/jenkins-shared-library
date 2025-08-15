c3840948c4d5da31ac65aec006d3096857508d47
sbiswas232 <139542832+sbiswas232@users.noreply.github.com>
2025-08-15 22:44:38 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,8 +1,5 @@
 def call(String ImageName, String ImageTag, String DockerhubUsr){
   sh"""
-  docker build -t "${DockerhubUsr}"/"${ImageName}":"${ImageTag}" .
-  #echo "${DOCKER_CREDENTIALS_PSW}" | docker login -u "${DockerhubUser}" --password-stdin docker.io
-  #docker push "${DOCKER_CREDENTIALS_USR}"/"${ImageName}":"${ImageTag}"
-  #docker logout
+  docker build -t "${DockerhubUsr}"\"${ImageName}":"${ImageTag}" .
   """
 }
